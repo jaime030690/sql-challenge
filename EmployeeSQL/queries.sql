39d5b243b9010ff761b06b62c5bc15843207f060
Jaime Echevarria <jaime030690@gmail.com>
2020-02-13 22:48:35 -0500

    fool me once...

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -62,6 +62,7 @@ WHERE dept_name = 'Sales' OR dept_name = 'Development';
 
 -- (8)	In descending order, list the frequency count of employee last names, i.e., 
 --		how many employees share each last name.
+
 SELECT last_name, count(*) AS "count"
 FROM employees
 GROUP BY last_name
